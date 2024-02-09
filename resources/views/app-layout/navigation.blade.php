dd9c8ef297038a5c62f8d401fcf6be46ea6717a1
king-hypertext <oseikin7@gmail.com>
2024-02-09 16:08:55 +0000

    first commit

--- resources/views/app-layout/navigation.blade.php
+++ resources/views/app-layout/navigation.blade.php
@@ -1,4 +1,18 @@
 <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
 <aside class="app-navigation">
     <h5 class="text-center my-1 fw-medium ">app navigation</h5>
+    <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
 </aside>
